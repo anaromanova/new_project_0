ce32af065ab7a25e37aa9712c2abe2ae08a00091
AnastasiaR <anastasia.romanova3169@gmail.com>
2024-09-26 20:06:45 +0300

    found out you need more than 2 commits(2)

--- src/processing.py
+++ src/processing.py
@@ -10,4 +10,4 @@ def filter_by_state(list_of_dicts: list[dict], state: str = 'EXECUTED') -> list[
 
 def sort_by_date(list_of_dicts: list[dict], ascending: bool = True) -> list[dict]:
     """Функция, которая возвращает новый список, отсортированный по дате."""
-    return sorted(list_of_dicts, key=lambda x: x['date'], reverse=ascending)+
+    return sorted(list_of_dicts, key=lambda x: x['date'], reverse=ascending)
