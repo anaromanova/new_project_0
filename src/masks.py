215b8ad73d0fbe78b769aa4101c28e56fb62c8e4
AnastasiaR <anastasia.romanova3169@gmail.com>
2024-10-21 18:45:07 +0300

    logging

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,20 @@
+import logging
+
+# Основная конфигурация logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='logs/masks.log',  # Запись логов в файл
+                    filemode='w')  # Перезапись файла при каждом запуске
+
+# Создаем логеры для различных компонентов программы
+get_mask_card_number_logger = logging.getLogger('app.get_mask_card_number')
+get_mask_account_logger = logging.getLogger('app.get_mask_account')
+
+
 def get_mask_card_number(number_of_card: str) -> str:
     """Функция, которая принимает на вход номер карты и возвращает ее маску."""
     if len(number_of_card) != 16 or not number_of_card.isdigit() or not isinstance(number_of_card, str):
+        get_mask_card_number_logger.warning('Возможна проблема с number_of_card')
         return ""
     else:
         index_of_first_number = 0
@@ -15,12 +29,15 @@ def get_mask_card_number(number_of_card: str) -> str:
                 lst_of_groups[i] = "".join(lst_of_groups[i][0:2]) + "**"
             elif i == 2:
                 lst_of_groups[i] = "****"
+        get_mask_card_number_logger.info('Успешное возвращение маски номера карты')
         return " ".join(lst_of_groups)
 
 
 def get_mask_account(number_of_account: str) -> str:
     """Функция, которая принимает на вход номер счета и возвращает его маску."""
     if len(number_of_account) != 20 or not number_of_account.isdigit() or not isinstance(number_of_account, str):
+        get_mask_account_logger.warning('Возможна проблема с number_of_account')
         return ""
     else:
+        get_mask_account_logger.info('Успешное возвращение маски номера счета')
         return "**" + number_of_account[-4:]
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,36 @@
 import json
+import logging
 import os
 
 import requests
 from dotenv import load_dotenv
+
+# Основная конфигурация logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='logs/utils.log',  # Запись логов в файл
+                    filemode='w')  # Перезапись файла при каждом запуске
+
+# Создаем логеры для различных компонентов программы
+reading_json_file_logger = logging.getLogger('app.reading_json_file')
+external_api_logger = logging.getLogger('app.external_api')
+amount_in_rub_logger = logging.getLogger('app.amount_in_rub')
 
 
 def reading_json_file(path: str) -> list:
     """Функция, которая принимает на вход путь до JSON-файла
     и возвращает список словарей с данными о финансовых транзакциях"""
     try:
+        reading_json_file_logger.info('Попытка открыть JSON-файл')
         with open(path, encoding='utf-8') as f:
             lst = json.load(f)
         if not isinstance(lst, list) or not lst:
+            reading_json_file_logger.warning('Проблема с содержимым JSON-файла')
             return []
         else:
             return lst
     except FileNotFoundError:
+        reading_json_file_logger.warning('Возможна проблема с путем до JSON-файла')
         return []
 
 
@@ -27,16 +42,22 @@ def external_api(amount: float, from_currency: str) -> dict:
         "apikey": f"{apikey}"
     }
     to_currency = 'RUB'
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
-    response = requests.get(url, headers=headers)
-    result = response.json()
-    return result
+    try:
+        external_api_logger.info('Попытка подключения через API')
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
+        response = requests.get(url, headers=headers)
+        result = response.json()
+        external_api_logger.info('Успешное подключение через API')
+        return result
+    except requests.exceptions.RequestException:
+        external_api_logger.warning('Возможна проблема с подключением через API')
 
 
 def amount_in_rub(transaction: dict) -> float:
     """Функция, которая принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
     amount = transaction['operationAmount']['amount']
     currency = transaction['operationAmount']['currency']['code']
+    external_api_logger.info('Попытка возвращения суммы транзакции в рублях')
     if currency == 'RUB':
         return amount
     else:
