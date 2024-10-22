7f903fbda1359580b539b17b88022fd1e5d593c1
AnastasiaR <anastasia.romanova3169@gmail.com>
2024-10-22 21:54:34 +0300

    fix homework

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,17 @@
 import logging
 
-# Основная конфигурация logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filename='logs/masks.log',  # Запись логов в файл
-                    filemode='w')  # Перезапись файла при каждом запуске
-
-# Создаем логеры для различных компонентов программы
-get_mask_card_number_logger = logging.getLogger('app.get_mask_card_number')
-get_mask_account_logger = logging.getLogger('app.get_mask_account')
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('logs/masks.log', mode='w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(number_of_card: str) -> str:
     """Функция, которая принимает на вход номер карты и возвращает ее маску."""
     if len(number_of_card) != 16 or not number_of_card.isdigit() or not isinstance(number_of_card, str):
-        get_mask_card_number_logger.warning('Возможна проблема с number_of_card')
+        logger.warning('Возможна проблема с number_of_card')
         return ""
     else:
         index_of_first_number = 0
@@ -29,15 +26,15 @@ def get_mask_card_number(number_of_card: str) -> str:
                 lst_of_groups[i] = "".join(lst_of_groups[i][0:2]) + "**"
             elif i == 2:
                 lst_of_groups[i] = "****"
-        get_mask_card_number_logger.info('Успешное возвращение маски номера карты')
+        logger.info('Успешное возвращение маски номера карты')
         return " ".join(lst_of_groups)
 
 
 def get_mask_account(number_of_account: str) -> str:
     """Функция, которая принимает на вход номер счета и возвращает его маску."""
     if len(number_of_account) != 20 or not number_of_account.isdigit() or not isinstance(number_of_account, str):
-        get_mask_account_logger.warning('Возможна проблема с number_of_account')
+        logger.warning('Возможна проблема с number_of_account')
         return ""
     else:
-        get_mask_account_logger.info('Успешное возвращение маски номера счета')
+        logger.info('Успешное возвращение маски номера счета')
         return "**" + number_of_account[-4:]
